8980fae9e81c2c1bd57ff5cabce11536cafc7a85
Pedro Alves <pedro22carro@gmail.com>
2020-11-13 17:50:55 -0300

    Participação de times finalizada

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade >= 18){
+                Console.WriteLine("Você gostaria de participar do time de CS GO? - Sim ou Não");
+                string resposta = Console.ReadLine();
+                if(resposta == "Sim"){
+                    Console.WriteLine("Compareça a secretaria - Falar com a SARA");
+                }else{
+                    Console.WriteLine("Blz! Passar bem.");
+                }
+            }else{
+                Console.WriteLine("Você gostaria de participar do Time de lol? Sim ou Não");
+                string resposta = Console.ReadLine();
+                if(resposta == "Sim"){
+                    Console.WriteLine("Compareça a secretaria - Falar com o Jucelino");
+                }else{
+                    Console.WriteLine("Muito Obrigado");
+                }
+            }
+        }
+    }
+}
